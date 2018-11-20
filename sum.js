e9eea55917c3cbd9f0c943ff59d1f0297694b6e2
Jonathan Nimmo <jnimmo@electronet.co.nz>
2018-11-21 10:59:55 +1300

    added error throwing to sum

--- sum.js
+++ sum.js
@@ -1,6 +1,9 @@
 exports.sum = function(){
     let sum = 0; 
     for(let number of arguments){
+        if(parseInt(sum) == NaN){
+            throw("sum() expects only numbers.")
+        }
         sum += Number(number)
     }
     return sum;
